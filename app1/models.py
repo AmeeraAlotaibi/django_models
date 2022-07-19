8f1302baddaeefd82a51559274a6c90fe6c1e3b6
AmeeraAlotaibi <meerareed.dev@outlook.com>
2022-07-19 20:38:55 +0300

    added model

---
+++ app1/models.py
@@ -0,0 +1,10 @@
+from email.policy import default
+from django.db import models
+
+# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(default= "")
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+    created_at = models.DateField(auto_now_add = True)
